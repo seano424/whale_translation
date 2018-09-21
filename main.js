57477c05dcb565ac188891d303fa429789e75045
Sean OReilly <soreilly424@gmail.com>
2018-09-21 11:20:31 -0500

    1st commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let input = 'A whale of a deal!';
+
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for(let i = 0; i < input.length; i++) {
+  for(let j = 0; j < vowels.length; j++){
+    if(input[i] === vowels[j]) {
+     resultArray.push(input[i]);
+      if(input[i] === 'e' || input[i] === 'u') {
+        resultArray.push(input[i]);
+      }
+    }
+  }
+}
+console.log(resultArray.join('').toUpperCase());
